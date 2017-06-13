9e0d35abaf792dda877acc4c370680a6abad411e
Thomas Huisking <tomasfaustin@gmail.com>
2017-06-13 14:08:49 -0700

    Add task filter, update data container, display incomplete tasks

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -36,7 +36,11 @@ class App extends Component {
     });
   }
   renderTasks() {
-    return this.props.tasks.map((task) => (
+    let filteredTasks = this.props.tasks;
+    if (this.state.hideCompleted) {
+      filteredTasks = filteredTasks.filter(task => !task.checked);
+    }
+    return filteredTasks.map((task) => (
       <Task key={task._id} task={task} />
     ));
   }
@@ -45,7 +49,7 @@ class App extends Component {
     return (
       <div className="container">
         <header>
-          <h1>Todo List</h1>
+          <h1>Todo List ({this.props.incompleteCount})</h1>
 
            <label className="hide-completed">
               <input
@@ -75,10 +79,12 @@ class App extends Component {
 
 App.propTypes = {
   tasks: PropTypes.array.isRequired,
+  incompleteCount: PropTypes.number.isRequired,
 };
 
 export default createContainer(() => {
   return {
     tasks: Tasks.find({}, { sort: { createdAt: -1 } }).fetch(),
+    incompleteCount: Tasks.find({ checked: { $ne: true } }).count(),
   };
 }, App);
